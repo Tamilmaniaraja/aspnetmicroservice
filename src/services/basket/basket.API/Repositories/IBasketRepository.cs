406b3aee64d13e4de60b80dbac5f975a2631d6db
Tamilmani Annamalai Raja <a.r.tamilmani@live.com>
2021-06-27 00:50:31 -0500

    Basket API Changes.

---
+++ src/services/basket/basket.API/Repositories/IBasketRepository.cs
@@ -0,0 +1,15 @@
+﻿using basket.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace basket.API.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string userName);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
+        Task DeleteBasket(string userName);
+    }
+}
